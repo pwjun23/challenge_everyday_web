05aadd3a3bc3f7c53cc5b80aafa4b7694ed63403
Wonjun <pwjun23@gmail.com>
2025-01-24 00:49:44 +0900

    월별 > 이름, 점수 색상 변경.

--- app/contents/Monthly.tsx
+++ app/contents/Monthly.tsx
@@ -85,7 +85,7 @@ const MonthlyView = (props : MonthlyViewProp) => {
                           total_point += task.task_point;
                         }
                       });
-                      const tag_name_point = total_point!==0?<div className={`float-left text-sm text-stone-600`} key={`${day}-${user_id}-${i}`}>{user_name} <b>{total_point}</b></div>:<div key={i}></div>;
+                      const tag_name_point = total_point!==0?<div className={`float-left text-sm text-stone-500`} key={`${day}-${user_id}-${i}`}>{user_name} <b className='text-blue-400'>{total_point}</b></div>:<div key={i}></div>;
                       return tag_name_point;
                     })}
                   </div>)
