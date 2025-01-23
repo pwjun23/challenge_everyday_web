5e47bdeb3b773d06dc324f7688f1ce0a5b8ba944
Wonjun <pwjun23@gmail.com>
2025-01-23 20:34:39 +0900

    스코어 줄나눔 디자인 추가.

--- app/contents/ScoreSheet.tsx
+++ app/contents/ScoreSheet.tsx
@@ -17,13 +17,15 @@ const ScoreSheet = (props : ScoreSheetProp) => {
   },[tasks_template])
 
   return (
-    <div className="p-4">
+    <div className="p-4 pb-20">
         {
-           tasks_template && selectedDate !== '' && Object.keys(tasks_template?.[selectedDate]).map((user_id,j)=>{
+           tasks_template && selectedDate !== '' && Object.keys(tasks_template?.[selectedDate]).sort((a:any,b:any)=> a<b?-1:a>b?1:0).map((user_id,j)=>{
                const idx = users_to_check.findIndex((user)=> user.user_id === user_id);
                const user_name = users_to_check[idx].user_name;
                 return (
-                    <div key={`score_sheet-${user_id}-${j}`} className="relative overflow-x-auto shadow-md sm:rounded-lg mb-4"><h3 className={`pb-2`} >{user_name}</h3>
+                <div key={`score_sheet-${user_id}-${j}`}>
+                    <div className="relative overflow-x-auto shadow-md sm:rounded-lg mb-4">
+                    <h3 className={`pl-2 pb-2`} >{user_name}<span className={`text-xs`}> 의 점수표</span></h3>
                     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                  <tr>
@@ -46,6 +48,15 @@ const ScoreSheet = (props : ScoreSheetProp) => {
                              </tbody>
                          </table>
                     </div>
+                    {Object.keys(tasks_template?.[selectedDate]).length-1 !== j && <div className="inline-flex items-center justify-center w-full">
+                        <hr className="w-64 h-1 my-8 bg-gray-200 border-0 rounded dark:bg-gray-700"/>
+                        <div className="absolute px-4 -translate-x-1/2 bg-gray-100 left-1/2 dark:bg-gray-900">
+                            <svg className="w-4 h-4 text-gray-700 dark:text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+                            <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
+                            </svg>
+                        </div>
+                    </div>}
+                </div>
                 )
         })
         }
