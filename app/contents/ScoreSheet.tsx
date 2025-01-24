dc811f4b905ce296a02de540b2376f8d29d9ddbd
wonjun <pwjun23@gmail.com>
2025-01-24 13:02:03 +0900

    버튼 제작

--- app/contents/ScoreSheet.tsx
+++ app/contents/ScoreSheet.tsx
@@ -2,13 +2,13 @@
 
 import React, { useEffect, useState } from 'react';
 import {ScoreSheetProp } from '../common_type';
-import _ from 'lodash';
+import _, { truncate } from 'lodash';
 
 const ScoreSheet = (props : ScoreSheetProp) => {
   const {checkLists} = props;
   const {tasks_template, users_to_check} = checkLists || {};
   const [selectedDate, setSelectedDate] = useState<string>('');
-
+  const [isEdit, setIsEdit] = useState<boolean>(false);
   
   useEffect(()=>{
     if(tasks_template){
@@ -25,7 +25,26 @@ const ScoreSheet = (props : ScoreSheetProp) => {
                 return (
                 <div key={`score_sheet-${user_id}-${j}`}>
                     <div className="relative overflow-x-auto shadow-md sm:rounded-lg mb-4">
-                    <h3 className={`pl-2 pb-2 text-stone-700`} >{user_name}<span className={`text-xs`}> 의 점수표</span></h3>
+                    <h3 className={`pl-2 pb-2 text-stone-700`} >{user_name}<span className={`text-xs`}> 의 점수표</span>
+                        {!isEdit && <button type="button" 
+                        onClick={()=> setIsEdit(true)}
+                        className="float-right text-gray-900 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-sm text-sm py-1 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2">
+                                            <svg className="w-4 h-4 text-gray-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1v3m5-3v3m5-3v3M1 7h7m1.506 3.429 2.065 2.065M19 7h-2M2 3h16a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1Zm6 13H6v-2l5.227-5.292a1.46 1.46 0 0 1 2.065 2.065L8 16Z"></path>
+                                            </svg>
+                                            <span className='sr-only'>수정</span>
+                                        </button>
+                        }
+                        {isEdit && <button type="button"
+                        onClick={()=> setIsEdit(false)}
+                        className="float-right text-gray-900 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-sm text-sm py-1 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2">
+                        <svg className="w-4 h-4 text-gray-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 8h11m0 0-4-4m4 4-4 4m-5 3H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h3"></path>
+                        </svg>
+                            <span className='sr-only'>수정</span>
+                        </button>}
+                    </h3>
+                    
                     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                  <tr>
@@ -37,11 +56,9 @@ const ScoreSheet = (props : ScoreSheetProp) => {
                              <tbody>
                     {tasks_template[selectedDate][user_id].map((task:any, k:any)=>{
                         return(<tr key={`task-${user_id}-${k}`} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                     {/* <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"></th> */}
                                      <td className="px-6 py-4">{task.task_name}</td>
                                      <td className="px-6 py-4">{task.task_point}</td>
                                      <td className="px-6 py-4">
-                                         {/* <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> */}
                                      </td>
                                  </tr>)
                     })}
