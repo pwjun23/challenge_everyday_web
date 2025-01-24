c7ed32aa089efc6396be14bc8612e36217f651cf
wonjun <pwjun23@gmail.com>
2025-01-24 10:42:10 +0900

    오타 수정.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "두더지가족 매일매일 습관 첼린지!",//"localhost:8080"
+  title: "두더지가족 매일매일 챌린지!",//"localhost:8080"
   description: "아이와 부모가 재밌게 습관 키우는 앱",
 };
 
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <meta name="apple-mobile-web-app-title" content="매일 매일 도전" />
+      <meta name="apple-mobile-web-app-title" content="매일 매일 챌린지" />
       <body
         className={`antialiased`}
       >
