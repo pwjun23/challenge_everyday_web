2fd3175619bffca45a124041d779592161e979ff
wonjun <pwjun23@gmail.com>
2025-08-08 15:25:43 +0900

    컴파일 에러 관련 수정.

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,4 +1,6 @@
-'use client';import React, { useState } from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
