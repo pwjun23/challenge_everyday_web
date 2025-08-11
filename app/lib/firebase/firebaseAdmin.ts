d2f361d052fa4cc30a87bf24bde372d90fd909b6
wonjun <pwjun23@gmail.com>
2025-08-11 17:01:25 +0900

    방어코드.

--- app/lib/firebase/firebaseAdmin.ts
+++ app/lib/firebase/firebaseAdmin.ts
@@ -3,6 +3,10 @@ import * as admin from 'firebase-admin';
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
 
+if (!serviceAccount) {
+  throw new Error('FIREBASE_SERVICE_ACCOUNT_KEY is not defined in environment variables.');
+}
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
